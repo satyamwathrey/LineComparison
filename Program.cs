9042ab013760228b364c713acb636e2ace31c273
satyamwathrey <swathrey1@gmail.com>
2022-03-30 23:06:53 +0530

    Welcome to the Line Comparison Program

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LineComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a program for Line Comparison");
+            Console.WriteLine();
+        }
+    }
+}
