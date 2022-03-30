73486e9fbe84d1ce980f8dfa8d7bed2d454b2f4e
satyamwathrey <swathrey1@gmail.com>
2022-03-31 00:20:24 +0530

    Added program for UC2

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace LineComparison
             Console.WriteLine("This is a program for Line Comparison");
             Console.WriteLine("Enter the coordinates"); 
 
-            double x1, y1, x2, y2, length;
+            double x1, y1, x2, y2, length1;
             
             Console.WriteLine("enter x1: ");
             x1=Convert.ToDouble(Console.ReadLine());
@@ -23,8 +23,28 @@ namespace LineComparison
             Console.WriteLine("enter y2: ");
             y2=Convert.ToDouble(Console.ReadLine());
 
-            length= Math.Sqrt(((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)));
-            Console.WriteLine("Length of line is: " + length);
+            length1= Math.Sqrt(((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)));
+
+            double a1, b1, a2, b2, length2;
+
+            Console.WriteLine("enter a1: ");
+            a1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("enter b1: ");
+            b1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("enter a2: ");
+            a2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("enter b2: ");
+            b2 = Convert.ToDouble(Console.ReadLine());
+
+            length2 = Math.Sqrt(((a2 - a1) * (a2 - a1)) + ((b2 - b1) * (b2 - b1)));
+
+            if(length1 == length2)
+            {
+                Console.WriteLine("Lines are of equal length");
+            }
             Console.ReadLine();
         }
     }
